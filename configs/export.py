eec61a9c4d9618897aa6443b3865d95e004b8ce5
ZombaSY <tjxhwl@gmail.com>
2025-03-17 01:31:18 +0000

    modify indent size of configuration

--- configs/export.py
+++ configs/export.py
@@ -1,29 +1,29 @@
 crop_size=128
 
 conf=dict(
-  env=dict(
-    debug=True,
-    CUDA_VISIBLE_DEVICES='0',
-    mode='export',
-    cuda=True,
-    task='classification',
-  ),
+    env=dict(
+        debug=True,
+        CUDA_VISIBLE_DEVICES='0',
+        mode='export',
+        cuda=True,
+        task='classification',
+    ),
 
-  model=dict(
-    name='Mobileone_s0_classification',
-    num_class=1000,
-    in_channel=3,
-    normalization='BatchNorm1d',
-    activation='ReLU',
-    dropblock=True,
-    use_ema=False,
-    input_size=[crop_size, crop_size],
-    saved_ckpt='pretrained/ImageNet/mobileone_s0_unfused.pth.tar',
-  ),
+    model=dict(
+        name='Mobileone_s0_classification',
+        num_class=1000,
+        in_channel=3,
+        normalization='BatchNorm1d',
+        activation='ReLU',
+        dropblock=True,
+        use_ema=False,
+        input_size=[crop_size, crop_size],
+        saved_ckpt='pretrained/ImageNet/mobileone_s0_unfused.pth.tar',
+    ),
 
-  export=dict(
-    target='onnx',
-    input_shape=[1, 3, crop_size, crop_size],
-    opset_version=12
-  )
+    export=dict(
+        target='onnx',
+        input_shape=[1, 3, crop_size, crop_size],
+        opset_version=12
+    )
 )
--- configs/train_segmentation.py
+++ configs/train_segmentation.py
@@ -2,92 +2,92 @@ input_size=[128, 128]   # (height, width)
 crop_size=128
 
 conf=dict(
-  env=dict(
-    debug=True,
-    CUDA_VISIBLE_DEVICES='0',
-    mode='train',
-    cuda=True,
-    wandb=True,
-    saved_model_directory='model_ckpt',
-    project_name='wandb_project',
-    task='segmentation',
-    train_fold=1,
-    epoch=10000,
-    early_stop_epoch=300,
-  ),
+    env=dict(
+        debug=True,
+        CUDA_VISIBLE_DEVICES='0',
+        mode='train',
+        cuda=True,
+        wandb=True,
+        saved_model_directory='model_ckpt',
+        project_name='wandb_project',
+        task='segmentation',
+        train_fold=1,
+        epoch=10000,
+        early_stop_epoch=300,
+    ),
 
-  model=dict(
-      name='Swin_t_semanticSegmentation_dsv',
-      num_class=20,
-      in_channel=3,
-      saved_ckpt='pretrained/ImageNet/swin_tiny_patch4_window7_224.pth',
-      use_ema=False,
-  ),
+    model=dict(
+        name='Swin_t_semanticSegmentation_dsv',
+        num_class=20,
+        in_channel=3,
+        saved_ckpt='pretrained/ImageNet/swin_tiny_patch4_window7_224.pth',
+        use_ema=False,
+    ),
 
-  dataloader_train=dict(
-    name='Image2Image',
-    mode='train',
-    data_path='/path/to/train.csv',
-    # data_path_folds=['/path/to/train-fold_0.csv',
-    #                  '/path/to/train-fold_1.csv',
-    #                  '/path/to/train-fold_2.csv',
-    #                  '/path/to/train-fold_3.csv',
-    #                  '/path/to/train-fold_4.csv'],
-    data_cache=True,
-    batch_size=32,
-    input_size=input_size,
-    workers=8,
+    dataloader_train=dict(
+        name='Image2Image',
+        mode='train',
+        data_path='/path/to/train.csv',
+        # data_path_folds=['/path/to/train-fold_0.csv',
+        #                  '/path/to/train-fold_1.csv',
+        #                  '/path/to/train-fold_2.csv',
+        #                  '/path/to/train-fold_3.csv',
+        #                  '/path/to/train-fold_4.csv'],
+        data_cache=True,
+        batch_size=32,
+        input_size=input_size,
+        workers=8,
 
-    augmentations=dict(
-      transform_blur=0.3,
-      transform_clahe=0.1,
-      transform_cutmix=0.8,
-      transform_coarse_dropout=0.2,
-      transform_fancyPCA=0.1,
-      transform_fog=0.01,
-      transform_g_noise=0.1,
-      transform_jitter=0.01,
-      transform_hflip=0.5,
-      transform_vflip=0.0,
-      transform_jpeg=0.5,
-      transform_perspective=0.1,
-      transform_rand_resize=0.7,
-      transform_rand_crop=crop_size,
-      transform_resize=input_size,
-      transform_rain=0.01,
-      transform_rotate=0.3,
-    )
-  ),
+        augmentations=dict(
+            transform_blur=0.3,
+            transform_clahe=0.1,
+            transform_cutmix=0.8,
+            transform_coarse_dropout=0.2,
+            transform_fancyPCA=0.1,
+            transform_fog=0.01,
+            transform_g_noise=0.1,
+            transform_jitter=0.01,
+            transform_hflip=0.5,
+            transform_vflip=0.0,
+            transform_jpeg=0.5,
+            transform_perspective=0.1,
+            transform_rand_resize=0.7,
+            transform_rand_crop=crop_size,
+            transform_resize=input_size,
+            transform_rain=0.01,
+            transform_rotate=0.3,
+        )
+    ),
 
-  dataloader_valid=dict(
-    name='Image2Image',
-    mode='valid',
-    data_path='/path/to/valid.csv',
-    # data_path_folds=['/path/to/valid-fold_0.csv',
-    #                  '/path/to/valid-fold_1.csv',
-    #                  '/path/to/valid-fold_2.csv',
-    #                  '/path/to/valid-fold_3.csv',
-    #                  '/path/to/valid-fold_4.csv'],
-    data_cache=True,
-    batch_size=32,
-    input_size=input_size,
-    workers=8,
-  ),
+    dataloader_valid=dict(
+        name='Image2Image',
+        mode='valid',
+        data_path='/path/to/valid.csv',
+        # data_path_folds=['/path/to/valid-fold_0.csv',
+        #                  '/path/to/valid-fold_1.csv',
+        #                  '/path/to/valid-fold_2.csv',
+        #                  '/path/to/valid-fold_3.csv',
+        #                  '/path/to/valid-fold_4.csv'],
+        data_cache=True,
+        batch_size=32,
+        input_size=input_size,
+        workers=8,
+    ),
 
-  criterion=dict(
-    name='CrossEntropyLoss'
-  ),
+    criterion=dict(
+        name='CrossEntropyLoss'
+    ),
 
-  optimizer=dict(
-    name='AdamW',
-    lr=1e-5,
-    lr_min=5e-7,
-    weight_decay=5e-3,
-  ),
+    optimizer=dict(
+        name='AdamW',
+        lr=1e-5,
+        lr_min=5e-7,
+        weight_decay=5e-3,
+    ),
 
-  scheduler=dict(
-    name='WarmupCosine',
-    cycles=50,
-    warmup_epoch=20
-  ),
+    scheduler=dict(
+        name='WarmupCosine',
+        cycles=50,
+        warmup_epoch=20
+    ),
 )
