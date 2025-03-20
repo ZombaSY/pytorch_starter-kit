06ed53989d5192fd2ca596e33a23c81b64c1fed1
ZombaSY <tjxhwl@gmail.com>
2025-03-20 07:43:59 +0000

    fix landmark layer on MLP.py

--- models/heads/MLP.py
+++ models/heads/MLP.py
@@ -6,21 +6,21 @@ from timm.models.layers import DropPath
 
 
 class SimpleLandmarker(nn.Module):
-    def __init__(self, channel_in, num_points=106, input_size=[128, 128]):
+    def __init__(self, channel_in, num_points=106, bottleneck_size=[128, 128]):
         super().__init__()
         self.reg_layer = nn.Sequential(*[
-            nn.Conv2d(channel_in, 128, kernel_size=1, stride=1, padding=1),
+            nn.Conv2d(channel_in, 128, kernel_size=1, stride=1, padding=0),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(128, 128, kernel_size=7, stride=3, padding=3),
+            nn.Conv2d(128, 128, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3),
             nn.Flatten(start_dim=1),
-            nn.Linear(128 * (input_size[0] // 64) * (input_size[1] // 64), num_points),
+            nn.Linear(128 * bottleneck_size[0] * bottleneck_size[1], num_points),
         ])
 
         self.apply(utils.init_weights)
 
-    def forward(self, x):
-        x_reg = self.reg_layer(x)
+    def forward(self, feat):
+        x_reg = self.reg_layer(feat)
 
         return x_reg
 
